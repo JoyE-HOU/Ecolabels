d22414b8785e10323be529c1a85778b6a78f8abf
Joy Evans <joyevans@me.com>
2021-09-13 20:02:00 -0500

    Formating Nav.js

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,15 +1,28 @@
-// import React, { Component } from 'react'
+import '../css/Nav.css'
+
+import {NavLink} from 'react-router-dom';
 
 function Nav() {
     return (
         <div className="Nav">
-            <div>Nav Message go to the 45:45 mark for details on creating buttons(HOME, FORM, FAQ, etc.)</div>
-            <div class="topnav">
-                <a class="active" href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+            <nav>
+                <div className="logo">
+                    <p>Nav Message go to the 45:45 mark for details on creating buttons(HOME, FORM, FAQ, etc.)</p>
+                </div>
+            <div className="topnav">
+                {/* option 1 */}
+                {/* <a><NavLink class="active" to="/home">Home</NavLink></a>
+                <a><NavLink to="/news">News</NavLink></a>
+                <a><NavLink to="/contact">Contact</NavLink></a>
+                <a><NavLink to="/about">About</NavLink></a> */}
+                
+                {/* option 2 */}
+                <a><NavLink to="/home">Home</NavLink></a>
+                <a><NavLink to="/news">News</NavLink></a>
+                <a><NavLink to="/contact">Contact</NavLink></a>
+                <a><NavLink to="/about">About</NavLink></a>
             </div>
+            </nav>
         </div>
     )
 }
