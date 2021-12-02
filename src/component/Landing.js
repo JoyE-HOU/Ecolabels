fab25412c1da50f05a2e74acc44a721c41a70bb9
Joy Evans <joyevans@me.com>
2021-12-02 11:56:12 -0600

    Editing CSS on landing section

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -5,12 +5,9 @@ function Landing() {
         <div className="Landing">
             <h1 id="title">Welcome to Ecolabels</h1>
             <h3>Ecolabels is a directory of ecolabels, tracking 8 ecolabels in the United States and 50 industry sectors.</h3>
-            {/* <div class="box-3"> */}
                 <div class="btn btn-three">
                     <button>Click Me to Find Your Ecolabel</button>
-                    {/* <span>Click Me to Find Your Ecolabel</span> */}
                 </div>
-            {/* </div> */}
         </div>
     )
 }
