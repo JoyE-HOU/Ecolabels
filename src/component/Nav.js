e07ab36792fc9105f3000f13c4c6b032d87aacdd
Joy Evans <joyevans@me.com>
2021-12-03 10:49:48 -0600

    Modified Nav bar

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -10,13 +10,6 @@ function Nav() {
                     <p>Nav Message go to the 45:45 mark for details on creating buttons(HOME, FORM, FAQ, etc.)</p>
                 </div>
             <div className="topnav">
-                {/* option 1 */}
-                {/* <a><NavLink class="active" to="/home">Home</NavLink></a>
-                <a><NavLink to="/news">News</NavLink></a>
-                <a><NavLink to="/contact">Contact</NavLink></a>
-                <a><NavLink to="/about">About</NavLink></a> */}
-                
-                {/* option 2 */}
                 <a><NavLink to="/home">Home</NavLink></a>
                 <a><NavLink to="/news">News</NavLink></a>
                 <a><NavLink to="/contact">Contact</NavLink></a>
